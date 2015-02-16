4fd3591ca5b83d325e267a9e00a3422bc34bf5b4
elsid <elsid.mail@gmail.com>
2015-02-16 22:14:03 +0300

    Refactor tests imports

--- src/match_uml/graph_matcher/test/__init__.py
+++ src/match_uml/graph_matcher/test/__init__.py
@@ -3,14 +3,10 @@
 
 from unittest import main
 
-from graph_matcher.test.node import (
-    MakeNodeTest as node_MakeNodeTest)
-from graph_matcher.test.graph import (
-    MakeGraphTest as graph_MakeGraphTest)
-from graph_matcher.test.configuration import (
-    MakeConfigurationTest as configuration_MakeConfigurationTest)
-from graph_matcher.test.match import (
-    MatchTest as match_MatchTest)
+from graph_matcher.test.node import MakeNodeTest
+from graph_matcher.test.graph import MakeGraphTest
+from graph_matcher.test.configuration import MakeConfigurationTest
+from graph_matcher.test.match import MatchTest
 
 if __name__ == '__main__':
     main()
--- src/match_uml/uml_matcher/test/__init__.py
+++ src/match_uml/uml_matcher/test/__init__.py
@@ -2,11 +2,8 @@
 
 from unittest import main
 
-from uml_matcher.test.type import (
-    ReprMultiplicityTest as type_ReprMultiplicityTest,
-    MakeTypeTest as type_MakeTypeTest)
-from uml_matcher.test.direction import (
-    MakeDirectionTest as direction_MakeDirectionTest)
+from uml_matcher.test.type import ReprMultiplicityTest, MakeTypeTest
+from uml_matcher.test.direction import MakeDirectionTest
 
 if __name__ == '__main__':
     main()
