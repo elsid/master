5e368ac723b88b7a764167eb11d00745df562345
elsid <elsid.mail@gmail.com>
2015-03-15 14:46:57 +0300

    Generate graph nodes in separate function

--- src/match_uml/graph_matcher/graph.py
+++ src/match_uml/graph_matcher/graph.py
@@ -4,45 +4,48 @@ from graph_matcher.node import Node
 from graph_matcher.match import match
 
 
+def generate_nodes(nodes_or_edges):
+    nodes_dict = {}
+
+    def get_node(node):
+        if isinstance(node, Node):
+            nodes_dict[id(node)] = node
+            return node
+        elif isinstance(node, (str, int, float)):
+            if node not in nodes_dict:
+                nodes_dict[node] = Node(node)
+            return nodes_dict[node]
+        else:
+            if id(node) not in nodes_dict:
+                nodes_dict[id(node)] = Node(node)
+            return nodes_dict[id(node)]
+
+    def make_edge(src_node, dst_node):
+        src_node = get_node(src_node)
+        dst_node = get_node(dst_node)
+        if src_node == dst_node:
+            src_node.self_connection = True
+        else:
+            src_node.connected_to.add(dst_node)
+            dst_node.connected_from.add(src_node)
+
+    nodes = set()
+    for node_or_edge in nodes_or_edges:
+        edge = node_or_edge
+        if isinstance(edge, (tuple, set, frozenset)) and len(edge) == 2:
+            src, dst = edge
+            if isinstance(edge, (set, frozenset)):
+                make_edge(dst, src)
+            make_edge(src, dst)
+        else:
+            nodes.add(get_node(node_or_edge))
+    nodes |= set(nodes_dict.values())
+    return nodes
+
+
 class Graph(object):
     def __init__(self, nodes_or_edges=tuple()):
-        nodes_dict = {}
-
-        def get_node(node):
-            if isinstance(node, Node):
-                nodes_dict[id(node)] = node
-                return node
-            elif isinstance(node, (str, int, float)):
-                if node not in nodes_dict:
-                    nodes_dict[node] = Node(node)
-                return nodes_dict[node]
-            else:
-                if id(node) not in nodes_dict:
-                    nodes_dict[id(node)] = Node(node)
-                return nodes_dict[id(node)]
-
-        def make_edge(src, dst):
-            src = get_node(src)
-            dst = get_node(dst)
-            if src == dst:
-                src.self_connection = True
-            else:
-                src.connected_to.add(dst)
-                dst.connected_from.add(src)
-
-        self.nodes = set()
-
-        for node_or_edge in nodes_or_edges:
-            edge = node_or_edge
-            if isinstance(edge, (tuple, set, frozenset)) and len(edge) == 2:
-                src, dst = edge
-                if isinstance(edge, (set, frozenset)):
-                    make_edge(dst, src)
-                make_edge(src, dst)
-            else:
-                self.nodes.add(get_node(node_or_edge))
-
-        self.nodes |= set(nodes_dict.values())
+        self.nodes = generate_nodes(nodes_or_edges)
 
     def match(self, pattern, limit=None):
         return match(self, pattern, limit)
