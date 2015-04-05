13f0d7b5b8e5fc6a623d8df6ba0addfc7be0a80f
elsid <elsid.mail@gmail.com>
2015-04-05 17:46:03 +0300

    Print by repr in uml_matcher example

--- src/match_uml/uml_matcher_example.py
+++ src/match_uml/uml_matcher_example.py
@@ -6,20 +6,9 @@ from uml_matcher.test.diagram import (
     DecoratorPatternDiagramFactory, TargetDiagramFactory)
 
 
-def print_graph_match_result(result):
-    for index, variant in enumerate(result):
-        print(index)
-        for mapping in variant:
-            print('\t', mapping)
-
-
-def print_uml_match_result(result):
-    print_graph_match_result(result.generalizations)
-    print_graph_match_result(result.associations)
-
 if __name__ == '__main__':
     pattern = DecoratorPatternDiagramFactory().diagram()
     target = TargetDiagramFactory().diagram()
-    print(pattern)
-    print(target)
-    print_uml_match_result(target.match(pattern))
+    print '\npattern\n\n', pattern
+    print '\ntarget\n\n', target
+    print '\nmatch\n\n', target.match(pattern)
