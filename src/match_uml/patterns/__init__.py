85b1b1a7f572b100dd72735ed609a879aa6c007d
elsid <elsid.mail@gmail.com>
2015-04-25 14:02:31 +0300

    Add function to make pattern diagram by name

--- src/match_uml/patterns/__init__.py
+++ src/match_uml/patterns/__init__.py
@@ -3,3 +3,22 @@
 from patterns.cached_method import cached_method
 from patterns.abstract_factory import AbstractFactory
 from patterns.decorator import Decorator
+
+PATTERNS = (
+    AbstractFactory,
+    Decorator,
+)
+
+
+class InvalidPatternName(Exception):
+    def __init__(self, name):
+        super(InvalidPatternName, self).__init__(
+            'invalid pattern name: %s' % name)
+        self.name = name
+
+
+def make_pattern(name):
+    patterns = {pattern.__name__: pattern for pattern in PATTERNS}
+    if name not in patterns:
+        raise InvalidPatternName(name)
+    return patterns[name]().diagram()
