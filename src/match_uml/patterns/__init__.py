d003e3c4df77bc4ec4427aeae9a2a689eb8364a8
elsid <elsid.mail@gmail.com>
2015-04-25 13:41:50 +0300

    Add AbstractFactory pattern

--- src/match_uml/patterns/__init__.py
+++ src/match_uml/patterns/__init__.py
@@ -1,4 +1,5 @@
 # coding: utf-8
 
 from patterns.cached_method import cached_method
+from patterns.abstract_factory import AbstractFactory
 from patterns.decorator import Decorator
---
+++ src/match_uml/patterns/abstract_factory.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+from uml_matcher import (
+    Class, Type, Operation, Generalization, Diagram, Interface)
+
+from patterns.cached_method import cached_method
+
+
+class AbstractFactory(object):
+    @cached_method
+    def abstract_factory(self):
+        return Interface('AbstractFactory', [], [self.product()])
+
+    @cached_method
+    def concrete_factory(self):
+        return Class('ConcreteFactory', [], [self.product()])
+
+    @cached_method
+    def abstract_product(self):
+        return Interface('AbstractProduct')
+
+    @cached_method
+    def abstract_product_type(self):
+        return Type(self.abstract_product())
+
+    @cached_method
+    def concrete_product(self):
+        return Class('ConcreteProduct')
+
+    @cached_method
+    def product(self):
+        Operation(self.abstract_product_type(), 'product')
+
+    @cached_method
+    def diagram(self):
+        return Diagram(
+            generalizations=[
+                Generalization(base=self.abstract_factory(),
+                               derived=self.concrete_factory()),
+                Generalization(base=self.abstract_product(),
+                               derived=self.concrete_product()),
+            ],
+        )
+
+
+if __name__ == '__main__':
+    print AbstractFactory().diagram()
