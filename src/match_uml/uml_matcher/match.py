e13bffd56b0ad5972759bf275921f5df14982ac2
elsid <elsid.mail@gmail.com>
2015-04-26 19:25:14 +0300

    Refactor match diagrams

--- src/match_uml/uml_matcher/match.py
+++ src/match_uml/uml_matcher/match.py
@@ -1,17 +1,61 @@
 # coding: utf-8
 
-from collections import namedtuple
+from copy import copy
 from graph_matcher import Graph, replace_node_by_obj
 
 
-BaseMatchVariant = namedtuple('BaseMatchVariant',
-                              ('generalizations', 'associations'))
+class Generalizations(tuple):
+    def has_correspondence_with_associations(self, associations):
+        gt_classifiers = (x.target for x in self)
+        at_classifiers = (x.target.type.classifier for x in associations)
+        gp_classifiers = (x.pattern for x in self)
+        ap_classifiers = (x.pattern.type.classifier for x in associations)
+        return (frozenset(gt_classifiers) >= frozenset(at_classifiers)
+                and frozenset(gp_classifiers) >= frozenset(ap_classifiers))
 
 
-class MatchVariant(BaseMatchVariant):
-    def __new__(cls, generalizations=tuple(), associations=tuple()):
-        return super(MatchVariant, cls).__new__(cls, tuple(generalizations),
-                                                tuple(associations))
+class Associations(tuple):
+    pass
+
+
+class MatchVariant(object):
+    def __init__(self,
+                 generalizations=tuple(),
+                 associations=tuple(),
+                 current_attr=None):
+        self.generalizations = Generalizations(generalizations)
+        self.associations = Associations(associations)
+        if current_attr:
+            self.current_attr = current_attr
+        elif self.generalizations:
+            self.current_attr = 'generalizations'
+        elif self.associations:
+            self.current_attr = 'associations'
+        else:
+            self.current_attr = None
+
+    def new(self, minor_variant):
+        self_copy = copy(self)
+        setattr(self_copy, minor_variant.current_attr, minor_variant.current)
+        return self_copy
+
+    def has_correspondence(self, minor_variant):
+        def get_func():
+            if isinstance(minor_variant.current, Generalizations):
+                return self.current.has_correspondence_with_generalizations
+            elif isinstance(minor_variant.current, Associations):
+                return self.current.has_correspondence_with_associations
+
+        return get_func()(minor_variant.current)
+
+    @property
+    def current(self):
+        return self.current_attr
+
+    @current.getter
+    def current(self):
+        assert hasattr(self, self.current_attr)
+        return getattr(self, self.current_attr)
 
     def __eq__(self, other):
         return (id(self) == id(other)
@@ -73,60 +117,57 @@ def eq_ignore_order(first, second):
 
 
 def match(target, pattern):
-    generalizations_variants = match_generalizations(target, pattern)
-    associations_variants = match_associations(target, pattern)
-    combined_variants = combine(generalizations_variants, associations_variants)
-    return MatchResult(combined_variants)
+    pipeline = (
+        match_generalizations,
+        match_associations
+    )
+
+    def process(combined_variants, func):
+        variants = func(target, pattern)
+        if combined_variants:
+            return combine(combined_variants, variants)
+        else:
+            return variants
+
+    return MatchResult(reduce(process, pipeline, None))
 
 
 def match_generalizations(target, pattern):
+    target_graph = Graph(
+        list(target.generalizations)
+        + list(associations_classifiers(target.associations)))
+    pattern_graph = Graph(
+        list(pattern.generalizations)
+        + list(associations_classifiers(pattern.associations)))
+    return [MatchVariant(generalizations=x)
+            for x in replace_node_by_obj(target_graph.match(pattern_graph))]
 
-    def generate_classifiers(associations):
-        for association in associations:
-            for end in association:
-                yield end.type.classifier
 
-    target_graph = Graph(list(target.generalizations)
-                         + list(generate_classifiers(target.associations)))
-    pattern_graph = Graph(list(pattern.generalizations)
-                          + list(generate_classifiers(pattern.associations)))
-    return replace_node_by_obj(target_graph.match(pattern_graph))
+def associations_classifiers(associations):
+    for association in associations:
+        for end in association:
+            yield end.type.classifier
 
 
 def match_associations(target, pattern):
     target_graph = Graph(target.associations)
     pattern_graph = Graph(pattern.associations)
-    return replace_node_by_obj(target_graph.match(pattern_graph))
+    return [MatchVariant(associations=x)
+            for x in replace_node_by_obj(target_graph.match(pattern_graph))]
 
 
-def combine(generalizations_variants, associations_variants):
-    if associations_variants:
-        if generalizations_variants:
+def combine(major_variants, minor_variants):
+    def add(result, minor_variant):
+        result += combine_one(major_variants, minor_variant)
+        return result
 
-            def add(result, x):
-                result += combine_association(generalizations_variants, x)
-                return result
-
-            return reduce(add, associations_variants, list())
-        else:
-            return (MatchVariant(associations=x) for x in associations_variants)
-    elif generalizations_variants:
-        return (MatchVariant(generalizations=x)
-                for x in generalizations_variants)
+    if minor_variants:
+        return reduce(add, minor_variants, list())
     else:
-        return tuple()
+        return major_variants
 
 
-def combine_association(generalizations_variants, associations_variant):
-    return (MatchVariant(generalizations=x, associations=associations_variant)
-            for x in generalizations_variants
-            if has_correspondence(x, associations_variant))
-
-
-def has_correspondence(generalizations_variant, associations_variant):
-    gt_classifiers = (x.target for x in generalizations_variant)
-    at_classifiers = (x.target.type.classifier for x in associations_variant)
-    gp_classifiers = (x.pattern for x in generalizations_variant)
-    ap_classifiers = (x.pattern.type.classifier for x in associations_variant)
-    return (frozenset(gt_classifiers) >= frozenset(at_classifiers)
-            and frozenset(gp_classifiers) >= frozenset(ap_classifiers))
+def combine_one(major_variants, minor_variant):
+    for major_variant in major_variants:
+        if major_variant.has_correspondence(minor_variant):
+            yield major_variant.new(minor_variant)
