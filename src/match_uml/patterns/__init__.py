ab24ea7e008d48657e1112d8e917b166796370df
elsid <elsid.mail@gmail.com>
2015-04-26 14:17:54 +0300

    Add pattern Bridge

--- src/match_uml/patterns/__init__.py
+++ src/match_uml/patterns/__init__.py
@@ -2,10 +2,12 @@
 
 from patterns.cached_method import cached_method
 from patterns.abstract_factory import AbstractFactory
+from patterns.bridge import Bridge
 from patterns.decorator import Decorator
 
 PATTERNS = (
     AbstractFactory,
+    Bridge,
     Decorator,
 )
 
