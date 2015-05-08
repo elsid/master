90293e13984b006ca9b44c43e9725bc13ad6195e
elsid <elsid.mail@gmail.com>
2015-05-09 00:00:24 +0300

    Use propety decorator for properties and operations of classifier

--- src/match_uml/uml_matcher/classifier.py
+++ src/match_uml/uml_matcher/classifier.py
@@ -10,33 +10,53 @@ class Classifier(NamedElement):
     def __init__(self, name='anonymous', properties=None,
                  operations=None, generals=None, suppliers=None):
         super(Classifier, self).__init__(name)
-        self.properties = list(properties) if properties else []
-        self.operations = list(operations) if operations else []
+        self.__properties = list(properties) if properties else []
+        self.__operations = list(operations) if operations else []
         self.generals = list(generals) if generals else []
         self.suppliers = list(suppliers) if suppliers else []
-        for property_ in self.properties:
+        for property_ in self.__properties:
             property_.owner = self
-        for operation in self.operations:
+        for operation in self.__operations:
+            operation.owner = self
+
+    @property
+    def properties(self):
+        return self.__properties
+
+    @properties.setter
+    def properties(self, value):
+        self.__properties = value
+        for property_ in self.__properties:
+            property_.owner = self
+
+    @property
+    def operations(self):
+        return self.__operations
+
+    @operations.setter
+    def operations(self, value):
+        self.__operations = value
+        for operation in self.__operations:
             operation.owner = self
 
     def has_property(self, name):
-        return name in set(property_.name for property_ in self.properties)
+        return name in set(property_.name for property_ in self.__properties)
 
     def has_operation(self, name):
-        return name in set(operation.name for operation in self.operations)
+        return name in set(operation.name for operation in self.__operations)
 
     @cached_eq
     def equiv_pattern(self, pattern):
         return (isinstance(pattern, Classifier)
-                and has_equivalents(self.properties, pattern.properties)
-                and has_equivalents(self.operations, pattern.operations))
+                and has_equivalents(self.__properties, pattern.__properties)
+                and has_equivalents(self.__operations, pattern.__operations))
 
     def __eq__(self, other):
         return (id(self) == id(other)
                 or super(Classifier, self).__eq__(other)
                 and isinstance(other, Classifier)
-                and eq_ignore_order(self.properties, other.properties)
-                and eq_ignore_order(self.operations, other.operations))
+                and eq_ignore_order(self.__properties, other.__properties)
+                and eq_ignore_order(self.__operations, other.__operations))
 
     def __str__(self):
         return 'classifier %s' % self.name
