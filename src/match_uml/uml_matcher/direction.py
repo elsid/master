dcfd05fda405e856a17c260d8cfc5223dfef854d
elsid <elsid.mail@gmail.com>
2015-05-10 19:59:47 +0300

    Use str constants for direction

--- src/match_uml/uml_matcher/direction.py
+++ src/match_uml/uml_matcher/direction.py
@@ -4,14 +4,9 @@ from enum import Enum
 
 
 class Direction(Enum):
-    in_ = 1
-    out = 2
-    inout = 3
+    in_ = 'in'
+    out = 'out'
+    inout = 'inout'
 
     def __str__(self):
-        if self == Direction.in_:
-            return 'in'
-        elif self == Direction.out:
-            return 'out'
-        elif self == Direction.inout:
-            return 'inout'
+        return self.value
