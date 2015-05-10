a835b80271f86e12dd2a5ab4ddcff88bb61d46bf
elsid <elsid.mail@gmail.com>
2015-05-10 19:29:34 +0300

    Add yaml representer and constructor for interface

--- src/match_uml/uml_matcher/test/interface.py
+++ src/match_uml/uml_matcher/test/interface.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import yaml
 from unittest import TestCase, main
 from hamcrest import assert_that, equal_to, starts_with
 from uml_matcher.interface import Interface
@@ -12,6 +13,13 @@ class MakeInterface(TestCase):
 
     def test_repr_should_succeed(self):
         assert_that(repr(Interface('A')), equal_to("Interface('A')"))
+
+    def test_dump_and_load_yaml_recursive_interface_should_succeed(self):
+        interface = Interface('a')
+        interface.suppliers = [interface]
+        data = "&id001 !Interface\nname: a\nsuppliers:\n- *id001\n"
+        assert_that(yaml.dump(interface), equal_to(data))
+        assert_that(yaml.load(data), equal_to(interface))
 
 
 if __name__ == '__main__':
