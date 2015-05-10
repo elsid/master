5ddae0a368f9b0d53ee64b2dcd6a7c01bb71dccd
elsid <elsid.mail@gmail.com>
2015-05-10 18:05:40 +0300

    Add yaml representer and constructor for direction

--- src/match_uml/uml_matcher/direction.py
+++ src/match_uml/uml_matcher/direction.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import yaml
 from enum import Enum
 
 
@@ -10,3 +11,16 @@ class Direction(Enum):
 
     def __str__(self):
         return self.value
+
+    @staticmethod
+    def yaml_representer(dumper, value):
+        return dumper.represent_scalar(u'!Direction', unicode(value))
+
+    @staticmethod
+    def yaml_constructor(loader, node):
+        value = loader.construct_scalar(node)
+        return Direction(value)
+
+
+yaml.add_representer(Direction, Direction.yaml_representer)
+yaml.add_constructor('!Direction', Direction.yaml_constructor)
