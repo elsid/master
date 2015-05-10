60a7d9ac26c262f29a2e01ac65b1545c808b2ca5
elsid <elsid.mail@gmail.com>
2015-05-10 19:28:57 +0300

    Add yaml representer and constructor for primitive type

--- src/match_uml/uml_matcher/primitive_type.py
+++ src/match_uml/uml_matcher/primitive_type.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import yaml
 from graph_matcher import cached_eq
 from uml_matcher.classifier import Classifier
 
@@ -13,3 +14,13 @@ class PrimitiveType(Classifier):
 
     def __str__(self):
         return 'primitive type %s' % self.name
+
+    @staticmethod
+    def yaml_constructor(loader, node):
+        result = PrimitiveType()
+        yield result
+        result.update(**loader.construct_mapping(node, True))
+
+
+yaml.add_representer(PrimitiveType, PrimitiveType.yaml_representer)
+yaml.add_constructor('!PrimitiveType', PrimitiveType.yaml_constructor)
--- src/match_uml/uml_matcher/test/primitive_type.py
+++ src/match_uml/uml_matcher/test/primitive_type.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import yaml
 from unittest import TestCase, main
 from hamcrest import assert_that, equal_to, starts_with
 from uml_matcher.primitive_type import PrimitiveType
@@ -13,6 +14,13 @@ class MakePrimitiveType(TestCase):
 
     def test_repr_should_succeed(self):
         assert_that(repr(PrimitiveType('A')), equal_to("PrimitiveType('A')"))
+
+    def test_dump_and_load_yaml_recursive_primitive_type_should_succeed(self):
+        primitive_type = PrimitiveType('a')
+        primitive_type.suppliers = [primitive_type]
+        data = "&id001 !PrimitiveType\nname: a\nsuppliers:\n- *id001\n"
+        assert_that(yaml.dump(primitive_type), equal_to(data))
+        assert_that(yaml.load(data), equal_to(primitive_type))
 
 
 if __name__ == '__main__':
