69123e6ca363b0c4b59811f326916971171c1f91
elsid <elsid.mail@gmail.com>
2015-05-16 14:07:59 +0300

    Check is model loaded from yaml

--- src/match_pattern/match_pattern.py
+++ src/match_pattern/match_pattern.py
@@ -6,6 +6,7 @@ from argparse import ArgumentParser
 from sys import stderr
 from os.path import isfile
 from java_parser import make_model
+from pattern_matcher import Model
 from patterns import make_pattern
 
 
@@ -31,7 +32,10 @@ def parse_args():
 def load_model(path_list, external_path_list):
     if (len(path_list) == 1 and isfile(path_list[0])
             and path_list[0].endswith('.yaml')):
-        return yaml.load(open(path_list[0]))
+        model = yaml.load(open(path_list[0]))
+        if not isinstance(model, Model):
+            raise InvalidYamlFormat(path_list[0])
+        return model
     model, errors = make_model(path_list=path_list,
                                external_path_list=external_path_list)
     if errors:
@@ -39,6 +43,14 @@ def load_model(path_list, external_path_list):
     for error in errors:
         print >> stderr, str(error)
     return model
+
+
+class InvalidYamlFormat(Exception):
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def __str__(self):
+        return 'invalid yaml format of file: "%s"' % self.file_path
 
 
 if __name__ == '__main__':
