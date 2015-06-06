aa44619826806fe958bf0e967d76668be19c7043
elsid <elsid.mail@gmail.com>
2015-06-06 12:27:56 +0300

    Simplify test

--- src/match_pattern/graph_matcher/test/match.py
+++ src/match_pattern/graph_matcher/test/match.py
@@ -138,8 +138,8 @@ class Match(TestCase):
         assert_that(actual_variants, contains_inanyorder(*expected_variants))
 
     def test_match_different_graphs_should_be_empty_result(self):
-        first = Graph({(1, 2), (2, 3), (3, 4)})
-        second = Graph({('a', 'b'), ('b', 'c'), ('c', 'a')})
+        first = Graph({(1, 2), (2, 3)})
+        second = Graph({('a', 'b'), ('b', 'a')})
         variants = to_list(replace_node_by_obj(match(first, second)))
         assert_that(variants, empty())
 
