94fa95574a337f354cf813aaf94fb6292212be6a
elsid <elsid.mail@gmail.com>
2015-06-07 14:07:59 +0300

    Print list of patterns models if name is not set

--- src/match_pattern/pattern_model.py
+++ src/match_pattern/pattern_model.py
@@ -9,13 +9,17 @@ from patterns import make_pattern, names
 
 def main():
     args = parse_args()
-    model = make_pattern(args.name)
-    yaml.dump(model, stdout)
+    if args.name:
+        model = make_pattern(args.name)
+        yaml.dump(model, stdout)
+    else:
+        for name in names():
+            print name
 
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('name', choices=tuple(names()))
+    parser.add_argument('name', nargs='?', choices=tuple(names()))
     return parser.parse_args()
 
 
