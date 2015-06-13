c5bb45311806214b1bb94289e42089f6de7e3d3d
elsid <elsid.mail@gmail.com>
2015-06-13 17:23:02 +0300

    Remove empty line

--- src/match_pattern/graph_matcher/match.py
+++ src/match_pattern/graph_matcher/match.py
@@ -116,7 +116,6 @@ class ConfigurationsGenerator(object):
             if not new_configurations_generated:
                 yield configuration
 
-
         self.__generators.append(generate(), priority())
         logging.debug('add generator %d with priority %d and checked len %d',
                       len(self.__generators), priority(),
