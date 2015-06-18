22cc773326f7464b07eec1f1e9ec5c675bde30a5
elsid <elsid.mail@gmail.com>
2015-06-18 20:59:06 +0300

    Remove unused code from cached_method

--- src/match_pattern/utils/cached_method.py
+++ src/match_pattern/utils/cached_method.py
@@ -4,24 +4,9 @@
 
 def cached_method(method):
 
-    def enable(self):
-        setattr(self, '__cache_enabled', True)
-
-    def disable(self):
-        setattr(self, '__cache_enabled', False)
-
-    def reset(self):
-        setattr(self, '__cache', {})
-
     def wrapper(self, *args, **kwargs):
         if not hasattr(self, '__cache'):
             setattr(self, '__cache', {})
-            setattr(self, 'enable_cache', enable)
-            setattr(self, 'disable_cache', disable)
-            setattr(self, 'reset_cache', reset)
-            setattr(self, '__cache_enabled', True)
-        elif not getattr(self, '__cache_enabled'):
-            return method(self, *args, **kwargs)
         cache = getattr(self, '__cache')
         if method in cache:
             return cache[method]
